f090484e1ed0d1b4e2cc42130add910285c67156
longmiao <454542297@qq.com>
2018-03-08 17:54:00 +0800

    yii中应用task

--- sr3/yii_task/protected/controllers/TestController.php
+++ sr3/yii_task/protected/controllers/TestController.php
@@ -2,7 +2,7 @@
 class TestController extends Controller{
 	public function actionTT(){
         $message['uid'] = 2;
-        $message['email'] = '83212019@qq.com';
+        $message['email'] = '454542297@qq.com';
         $message['title'] = '接口报警邮件';
         $message['contents'] = "'EmailEvent'接口请求过程出错！ 错误信息如下：err_no:'00000' err_msg:'测试队列' 请求参数为:'[]'";
         $message['type'] = 2;
